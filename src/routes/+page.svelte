164e92feb2c7fd1c87343f283f04ee421e889eca
Bilele <bilele.elhaddadi@gmail.com>
2026-08-17 14:57:28 +0200

    chore: added colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<h1>Welcome to your library project</h1>
+<h1>teletype-ui</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="https://sveltvar(--size-s3)e.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div style="weight: 20px; height: 20px; background-color: var(--primary-20)">
+</div>
