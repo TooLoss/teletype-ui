c730d6bc2d411544d1d952e37d69f6cf874f17f8
Bilele <bilele.elhaddadi@gmail.com>
2026-08-17 14:32:08 +0200

    chore: imported font and spacing

---
+++ src/lib/types.d.ts
@@ -0,0 +1,1 @@
+export type Size = `s1` | `s2` | `s3` | `s4` | `s5` | `zero`;
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import '$lib/styles/tokens.css';
+    import '$lib/styles/base.css';
+
+    let { children } = $props();
+</script>
+
+<div class="container">
+    {@render children?.()}
+</div>
+
+<style>
+    .container {
+        max-width: min(1000px, 100%);
+        margin: 0 auto;
+    }
+</style>
+
