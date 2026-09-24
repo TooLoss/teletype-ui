81392ac9a91076fe7add46cfd26dd8c2457b73ca
Loss <bilele.elhaddadi@gmail.com>
2026-09-24 22:44:40 +0200

    feat(Picker): begin color picker

---
+++ src/lib/internal/ColorPicker.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import Frame from '../components/Frame.svelte';
+    import Stack from '../layouts/Stack.svelte';
+
+    interface Props {
+        color?: number;
+        chroma?: number;
+    }
+
+    let {
+        color = $bindable(250),
+        chroma = $bindable(0.15)
+    }: Props = $props();
+</script>
+
+<div class="square">
+    <div class="picker">
+    </div>
+</div>
+
+<style>
+    .square {
+        width: 100px;
+        height: 100px;
+        background-color: var(--bg-app-variant);
+        overflow: hidden;
+        transform: translateZ(0); 
+    }
+
+    .picker {
+        width: 100%;
+        height: 100%;
+        background:
+            conic-gradient(
+                from 0deg in oklch longer hue,
+                oklch(70% 0.25 0deg),
+                oklch(70% 0.25 360deg)
+            );
+        filter: blur(10px);
+        transform: scale(1.3);
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,21 @@
     import '$lib/styles/tokens.css';
     import '$lib/styles/base.css';
 
+    import ColorPicker from '$lib/internal/ColorPicker.svelte';
+
+    let color = $state(250);
+    let chroma = $state(0.15);
+
     let { children } = $props();
+
+    $effect(() => {
+        if (typeof document !== 'undefined') {
+            document.documentElement.style.setProperty('--hue-primary', `${color}deg`);
+            document.documentElement.style.setProperty('--primary-base-chroma', `${chroma}`);
+        }
+    });
 </script>
 
-<div class="page">
-    {@render children?.()}
-</div>
+<ColorPicker bind:color bind:chroma />
 
-<style>
-    .page {
-        margin: 0 auto;
-    }
-</style>
-
+{@render children?.()}
